525b73e414ec9156177dfacc2548ba62b066b667
ninvandung1996 <ninhvandung1996@gmail.com>
2019-02-13 09:56:28 +0700

    firebase

---
+++ src/containers/App/firebase.js
@@ -0,0 +1,16 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD2jNSQ-nJBI0yKdd4tv0_nSKADrxXLGu0",
+    authDomain: "vin-leasing-123.firebaseapp.com",
+    databaseURL: "https://vin-leasing-123.firebaseio.com",
+    projectId: "vin-leasing-123",
+    storageBucket: "vin-leasing-123.appspot.com",
+    messagingSenderId: "620773447236"
+};
+
+firebase.initializeApp(firebaseConfig);
+export const messaging = firebase.messaging();
+
+// Add the public key generated from the console here.
+messaging.usePublicVapidKey(
+    "BCTJ4XMqSZYgGQ4fgkYHHLUUe0u3brLwLRAsMhQM8wHfAEvlEPsBPk4ydy8y8lvH2YZ1phhnC-ivxNp8uLzO2SA"
+);
