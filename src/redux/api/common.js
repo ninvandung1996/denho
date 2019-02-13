35ce6387c0fc77fc1c9fe80da417ed0fc197b063
Duy Linh <duylinh196tb@gmail.com>
2019-02-13 09:34:21 +0700

    fix

--- src/redux/api/common.js
+++ src/redux/api/common.js
@@ -2,22 +2,22 @@ import { create } from "apisauce";
 import { API_TIMEOUT } from "../constants/api";
 import configs from "../constants/configs";
 
-let endpoint = "";
-const node_env = process.env.NODE_ENV;
-if (
-  node_env !== "dev" &&
-  node_env !== "development" &&
-  node_env !== "develop"
-) {
-  endpoint = configs.endPoint;
-  console.log("object");
-} else endpoint = configs.endPoint;
-console.log({
-  node_env,
-  endpoint,
-  check: node_env === "development",
-  port: process.env.PORT
-});
+const endpoint = configs.endPointLocal;
+// const node_env = process.env.NODE_ENV;
+// if (
+//   node_env !== "dev" &&
+//   node_env !== "development" &&
+//   node_env !== "develop"
+// ) {
+//   endpoint = configs.endPoint;
+//   console.log("object");
+// } else endpoint = configs.endPoint;
+// console.log({
+//   node_env,
+//   endpoint,
+//   check: node_env === "development",
+//   port: process.env.PORT
+// });
 
 const endpointImage = `${endpoint}/uploads/files/image`;
 const API = create({
