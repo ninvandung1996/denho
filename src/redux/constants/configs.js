b50a22f7a0b52d19a52537c1df9a2ed43b3997f5
ninvandung1996 <ninhvandung1996@gmail.com>
2019-03-01 13:57:18 +0700

    fix endpointimage

--- src/redux/constants/configs.js
+++ src/redux/constants/configs.js
@@ -1,7 +1,7 @@
 export default {
   endPoint: "http://3.0.89.212:2222/api/v1",
   endPointLocal: "http://192.168.1.6:2000/api/v1",
-  endPointImage: "http://192.168.1.6:2000/api/v1/uploads/files/",
+  endPointImage: "http://3.0.89.212:2222/api/v1/uploads/files/",
   appId: "7816cdf418aa2f9815dbe39b63dba5e5a6a4658933d061e6686ba4ad28f1852f",
   appSecret: "b31380d507f48104cc1cf6e80f2c0aaa42547da8d1a0f4d0fcd84b524655efc0",
   socketUrl: "http://localhost:4000/api/v1"
